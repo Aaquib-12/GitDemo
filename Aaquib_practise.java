e5267552e09c0f54b4df6cd377d93d2cb1d0ecae
Aaquib <aaqibsh10@gmail.com>
2023-09-14 19:45:00 +0530

    first commit

---
+++ Aaquib_practise.java
@@ -0,0 +1,13 @@
+class Aaquib_practise
+{
+static
+{
+System.out.println("Hello");
+}
+
+public static void main(String args [])
+{
+System.out.println("Welcome to programming world");
+System.out.println(10/2);
+}
+}
